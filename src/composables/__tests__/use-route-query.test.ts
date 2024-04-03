13ec46c083c078671f0f60703de5a247d9cef27f
hungify <nmhungify@gmail.com>
2024-04-03 22:18:58 +0700

    Refactor useRouteQuery to include debounce functionality

--- src/composables/__tests__/use-route-query.test.ts
+++ src/composables/__tests__/use-route-query.test.ts
@@ -1,34 +1,58 @@
-import { type MockedFunction, describe, expect, it, vi } from 'vitest'
-import { useRoute } from 'vue-router'
+import { type MockedFunction, describe, expect, it } from 'vitest'
+import { mount } from '@vue/test-utils'
 import { useRouteQuery } from '../use-route-query'
 
 vi.mock('vue-router')
 
 const mockUseRoute = useRoute as MockedFunction<typeof useRoute>
+const mockUseRouter = useRouter as MockedFunction<typeof useRouter>
 
 describe('useRouteQuery', () => {
-  mockUseRoute.mockReturnValue({
-    fullPath: '/?name=John&age=20',
-    query: { name: 'John', age: '20' },
-    hash: '',
-    params: {},
-    matched: [],
-    meta: {},
-    name: '/',
-    path: '/',
-    redirectedFrom: undefined,
+  beforeEach(() => {
+    mockUseRoute.mockReturnValue({
+      path: '/example',
+      query: { name: 'John', age: '20' },
+      fullPath: '/?name=John&age=20',
+    } as any)
+
+    mockUseRouter.mockReturnValue({
+      push: vi.fn(),
+      replace: vi.fn(),
+    } as any)
   })
-
-  it('should return valRouteQuery and fnRouteQuery', () => {
-    const { valRouteQuery, fnRouteQuery } = useRouteQuery()
-
-    expect(valRouteQuery.value).toEqual({ name: 'John', age: '20' })
-
-    expect(fnRouteQuery.value).toEqual({
-      name: expect.any(Function),
-      age: expect.any(Function),
+  it('should update route query when route changes', () => {
+    mockUseRoute.mockReturnValue({
+      fullPath: '/?name=John&age=20',
+      query: { name: 'John', age: '20' },
+      hash: '',
+      params: {},
+      matched: [],
+      meta: {},
+      name: '/',
+      path: '/',
+      redirectedFrom: undefined,
     })
-    expect(fnRouteQuery.value.name()).toBe('John')
-    expect(fnRouteQuery.value.age()).toBe('20')
+
+    const { routeQuery, reset, debounce } = useRouteQuery({
+      name: '/',
+      ms: 500,
+      method: 'replace',
+    })
+
+    // Assert initial values
+    expect(routeQuery.value).toEqual({ name: 'John', age: '20' })
+
+    // Call reset and assert values
+    reset()
+    expect(routeQuery.value).toEqual({})
+
+    // Call debounce and assert values
+    debounce({ param1: 'newvalue1', param2: 'newvalue2' })
+    setTimeout(() => {
+      expect(routeQuery.value).toEqual({
+        param1: 'newvalue1',
+        param2: 'newvalue2',
+      })
+    }, 500)
   })
 })
--- src/composables/use-route-query.ts
+++ src/composables/use-route-query.ts
@@ -1,28 +1,74 @@
+import { type PromisifyFn, useDebounceFn, watchPausable } from '@vueuse/core'
 import { type ParsedQuery, getQuery } from 'ufo'
+import type { RouteNamedMap } from 'vue-router/auto/routes'
+import type { UnwrapRef } from 'vue'
 
 type FnRouteQuery<T extends NonNullable<unknown>> = {
   [P in keyof T]: () => NonNullable<T[P]>
 }
 
-export const useRouteQuery = <T extends ParsedQuery>() => {
-  const route = useRoute()
+type UseRouteQueryArgs = {
+  ms?: number
+  name: keyof RouteNamedMap
+  method?: 'replace' | 'push'
+}
+
+export const useRouteQuery = <T extends ParsedQuery>({
+  name,
+  ms = 0,
+  method = 'replace',
+}: UseRouteQueryArgs) => {
+  const route = useRoute(name)
+  const router = useRouter()
+  const routeQuery = ref(getQuery<T>(route.fullPath))
 
   const fnRouteQuery = computed(() => {
-    return objectKeys(valRouteQuery.value).reduce<FnRouteQuery<T>>(
+    return objectKeys(routeQuery.value).reduce<FnRouteQuery<T>>(
       (acc, key) => ({
         ...acc,
-        [key]: () => valRouteQuery.value[key]?.toString(),
+        [key]: () => routeQuery.value[key]?.toString(),
       }),
       {} as FnRouteQuery<T>,
     )
   })
 
-  const valRouteQuery = computed(() => {
-    return getQuery<T>(route.fullPath)
-  })
+  const { pause, resume } = watchPausable(
+    routeQuery,
+    (newRouteQuery) => {
+      router[method]({
+        query: newRouteQuery,
+      })
+    },
+    {
+      deep: true,
+      immediate: true,
+    },
+  )
+
+  const reset = () => {
+    routeQuery.value = getQuery<T>(route.path) as unknown as UnwrapRef<T>
+  }
+
+  const debounce = useDebounceFn(
+    async <Q extends T>(
+      queryObj: Q,
+      method: 'replace' | 'push' = 'replace',
+    ) => {
+      pause()
+      router[method]({
+        query: queryObj,
+      })
+      routeQuery.value = queryObj as unknown as UnwrapRef<T>
+      await nextTick()
+      resume()
+    },
+    ms,
+  ) satisfies PromisifyFn<<Q extends T>(queryObj: Q) => void>
 
   return {
-    valRouteQuery,
+    routeQuery,
     fnRouteQuery,
+    reset,
+    debounce,
   }
 }
