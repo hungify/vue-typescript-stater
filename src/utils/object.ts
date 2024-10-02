f2b6d31fba74a190c0d20dd2cfde506ff4e1be00
hungify <nmhungify@gmail.com>
2024-10-02 21:46:08 +0700

    feat: add pick and omit

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -3,3 +3,22 @@ export const objectKeys = <Obj extends NonNullable<unknown>>(
 ): Array<keyof Obj> => {
   return Object.keys(obj) as Array<keyof Obj>
 }
+
+export function pick<T, K extends keyof T>(object: T, keys: K[]): Pick<T, K> {
+  return Object.assign(
+    {},
+    // eslint-disable-next-line array-callback-return
+    ...keys.map((key) => {
+      if (object && Object.prototype.hasOwnProperty.call(object, key))
+        return { [key]: object[key] }
+    }),
+  )
+}
+
+export function omit<T extends object, K extends keyof T>(
+  obj: T,
+  ...keys: K[]
+): Omit<T, K> {
+  keys.forEach((key) => delete obj[key])
+  return obj
+}
