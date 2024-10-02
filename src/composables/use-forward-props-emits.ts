280674f732828490eb73f8fad584c46bf3bb88ec
hungify <nmhungify@gmail.com>
2024-10-02 21:46:18 +0700

    feat: Add composable functions for context, emit as props, forward expose, forward props, and forward ref

---
+++ src/composables/use-forward-props-emits.ts
@@ -0,0 +1,28 @@
+import { computed, type MaybeRefOrGetter } from 'vue'
+import { useEmitAsProps } from './use-emit-as-props'
+import { useForwardProps } from './use-forward-props'
+
+/**
+ * The function `useForwardPropsEmits` takes in props and an optional emit function, and returns a
+ * computed object that combines the parsed props and emits as props.
+ * @param {T} props - The `props` parameter is of type `T`, which is a generic type that extends the
+ * parameters of the `useForwardProps` function. It represents the props object that is passed to the
+ * `useForwardProps` function.
+ * @param [emit] - The `emit` parameter is a function that can be used to emit events. It takes two
+ * arguments: `name`, which is the name of the event to be emitted, and `args`, which are the arguments
+ * to be passed along with the event.
+ * @returns a computed property that combines the parsed
+ * props and emits as props.
+ */
+export function useForwardPropsEmits<
+  T extends Record<string, any>,
+  Name extends string,
+>(props: MaybeRefOrGetter<T>, emit?: (name: Name, ...args: any[]) => void) {
+  const parsedProps = useForwardProps(props)
+  const emitsAsProps = emit ? useEmitAsProps(emit) : {}
+
+  return computed(() => ({
+    ...parsedProps.value,
+    ...emitsAsProps,
+  }))
+}
